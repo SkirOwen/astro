82ccd9b3362988b37d20ce68132e0a3c2f32e906
Owen <owen.allemang@gmail.com>
2020-05-07 12:14:55 +0200

    perfected flat field function

--- gaia.py
+++ gaia.py
@@ -2,8 +2,10 @@ import numpy as np
 from astropy.io import fits
 # import matplotlib.pyplot as plt
 import os
+import time
 
 directory = "gaia-606"
+output_dir = "CORRECTED_gaia-606"
 
 all_images_name_list = os.listdir(directory)
 
@@ -14,27 +16,33 @@ gaia_lst_files = [os.path.join(directory, name) for name in all_images_name_list
 # offset
 hdu_offset = [fits.open(images) for images in offset_lst_files]
 data_list_offset = [images[0].data for images in hdu_offset]
-master_offset = np.mean(data_list_offset, axis=0)
+master_offset = np.median(data_list_offset, axis=0)
 
 # flat
 hdu_flat = [fits.open(images) for images in flat_lst_files]
 data_list_flat = [images[0].data for images in hdu_flat]
-master_flat = np.mean(data_list_flat, axis=0)
+master_flat = np.median(data_list_flat, axis=0)
 
 
 def flat_field(images_index):
     global master_flat
     global master_offset
+
     im = fits.open(gaia_lst_files[images_index])
     data_im = im[0].data
     data_im_new = ((data_im - master_offset) * np.mean(master_flat)) / (master_flat - master_offset)
-    fits.writeto(("image_corrected" + str(images_index).zfill(3) + ".fits"), data_im_new, im[0].header)
-    print("image_corrected" + str(images_index).zfill(3) + ".fits" + " has been created")
+    name = ("image_corrected_" + str(images_index+1).zfill(3) + ".fits")
+    fits.writeto(os.path.join(output_dir, name), data_im_new, im[0].header)
+    print("image_corrected_" + str(images_index+1).zfill(3) + ".fits" + " has been created")
     return
 
 
 if __name__ == "__main__":
-    flat_field(0)
+    start_time = time.time()
+    for i in range(len(gaia_lst_files)):
+        flat_field(i)
+    end_time = time.time()
+    print("Completed in: ", (end_time - start_time))
 
 # data_im_new, im = flat_field(0)
 #
